ab6ca62d1502fbd71110538ba66ac205456336a6
emirkaan <emirkaansaricam@gmail.com>
2025-01-22 23:32:24 +0300

    page update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import { Hero } from "@/components/Hero";
-import Listing from "@/components/HomeListing";
+import HomeListing from "@/components/HomeListing";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Listing />
+      <HomeListing />
     </>
   );
 }
