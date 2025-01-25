e3ab85b51cf65f4574bf2bbb65a637760944f4b1
emirkaan <emirkaansaricam@gmail.com>
2025-01-25 23:16:42 +0300

    shoplisting update

--- src/components/ShopListing.jsx
+++ src/components/ShopListing.jsx
@@ -4,7 +4,7 @@ import { ShopListItemCard } from "./ShopListItemCard";
 
 export const ShopListing = () => {
   return (
-    <div className="bg-gray-100 py-10">
+    <div className="py-10">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {data.map((doll) => (
