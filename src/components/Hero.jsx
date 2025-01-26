c7840b8a945b8b6aa5f5ad028a0bcd4c7a8bb687
emirkaan <emirkaansaricam@gmail.com>
2025-01-26 21:59:46 +0300

    hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,24 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <section className="relative w-full h-[92dvh]">
-      <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-black/50 lg:to-transparent z-10"></div>
+    <section className="relative h-[92dvh] w-full bg-black/80">
+      <div className="absolute inset-0 z-10 bg-black/50 lg:to-transparent"></div>
 
       <img
-        src="/assets/hero-image.webp"
+        src="/assets/hero-test-2.png"
         alt="Hero"
-        className="object-cover w-full h-full"
+        className="h-full w-full object-cover"
       />
 
-      <div className="absolute mx-2 bg-black/40 rounded-2xl flex flex-col z-20 p-10 top-1/2 md:w-1/2 xl:w-1/3 2xl:w-1/4 font-medium md:mx-4 -translate-y-1/2  text-rose-50">
-        <h1 className="text-5xl lg:text-6xl xl:text-7xl mb-4 text-center lg:text-start leading-10">
+      <div className="absolute top-1/2 z-20 mx-2 flex -translate-y-1/2 flex-col rounded-2xl bg-black/40 p-10 font-medium text-rose-50 md:mx-4 md:w-1/2 xl:w-1/3 2xl:w-1/4">
+        <h1 className="mb-4 text-center text-5xl leading-10 lg:text-start lg:text-6xl xl:text-7xl">
           Crafted with Love, Made from Nature
         </h1>
-        <p className="text-xl italic text-center lg:text-start lg:text-2xl">
+        <p className="text-center text-xl italic lg:text-start lg:text-2xl">
           Soft, safe, and sustainably handmade for your child’s endless
           imagination.
         </p>
-        <button className="py-2 px-4 text-xl mt-4 self-center bg-rose-300 text-stone-800 font-semibold rounded-2xl hover:bg-rose-400 transition-all duration-500">
+        <button className="mt-4 self-center rounded-2xl bg-rose-300 px-4 py-2 text-xl font-semibold text-stone-800 transition-all duration-500 hover:bg-rose-400">
           <Link href="/shop">Go to Shop</Link>
         </button>
       </div>
