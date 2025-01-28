45d7c2d19e45894a2e4e7a77451b4b2fcfb4a214
emirkaan <emirkaansaricam@gmail.com>
2025-01-28 21:28:55 +0300

    wishlist page created

---
+++ src/app/wishlist/page.jsx
@@ -0,0 +1,10 @@
+import { WishlistListing } from "@/components/WishlistListing";
+
+export const metadata = {
+  title: "My Wishlist",
+  description: "Explore the collection of handmade dolls",
+};
+
+export default function Page() {
+  return <WishlistListing />;
+}
